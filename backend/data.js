4d5ca2f5d5732ffbda3a1f014e2ce2864269d283
AbhinashKumarSingh <singhabhinash840@gmail.com>
2021-09-01 15:34:11 +0530

    Server is created in backend

---
+++ backend/data.js
@@ -0,0 +1,67 @@
+
+const data ={
+    products:[
+        {
+        __id:'1',
+        name:'Sparky Jeans',
+        category:'Jeans',
+        image:'/images/p1.jpg',
+        price:120,
+        countInStock:10,
+        brand:"Sparky",
+        rating:4.5,
+        numReviews:10,
+        description:'high quality product'
+    },
+    {
+        __id:'2',
+        name:'Netplay Jeans',
+        category:'Jeans',
+        image:'/images/p2.jpg',
+        price:120,
+        countInStock:20,
+        brand:"Netplay",
+        rating:4.5,
+        numReviews:10,
+        description:'high quality product'
+    },
+    {
+        __id:'3',
+        name:'Calvin Jeans',
+        category:'Jeans',
+        image:'/images/p4.jpg',
+        price:120,
+        countInStock:30,
+        brand:"Calvin",
+        rating:4.5,
+        numReviews:10,
+        description:'high quality product'
+    },
+    {
+        __id:'4',
+        name:'Jockey Jeans',
+        category:'Jeans',
+        image:'/images/p4.jpg',
+        price:120,
+        countInStock:0,
+        brand:"Jockey",
+        rating:4.5,
+        numReviews:10,
+        description:'high quality product'
+    },
+    {
+        __id:'5',
+        name:'Adidas Jeans',
+        category:'Jeans',
+        image:'/images/p5.jpg',
+        price:120,
+        countInStock:1,
+        brand:"Adidas",
+        rating:4.5,
+        numReviews:10,
+        description:'high quality product'
+    }
+]
+}
+
+export default data;
